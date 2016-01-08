4db09110f156f339f9841acad8646038e97d9498
Weitao Zhou <wtzhou@dataman-inc.com>
2016-01-08 08:35:42 +0800

    Typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed "s/HDFS_NAMENODE_RPC_HOST/$HDFS_NAME_NODE_RPC_HOST/;s/HDFS_NAME_NODE_RPC_PORT/$HDFS_NAME_NODE_RPC_PORT/" ${HADOOP_INSTALL_DIR}/etc/hadoop/core-site.xml.template > ${HADOOP_INSTALL_DIR}/etc/hadoop/core-site.xml
+sed "s/HDFS_NAMENODE_RPC_HOST/$HDFS_NAMENODE_RPC_HOST/;s/HDFS_NAMENODE_RPC_PORT/$HDFS_NAMENODE_RPC_PORT/" ${HADOOP_INSTALL_DIR}/etc/hadoop/core-site.xml.template > ${HADOOP_INSTALL_DIR}/etc/hadoop/core-site.xml
 
 cat ${HADOOP_INSTALL_DIR}/etc/hadoop/core-site.xml
 
